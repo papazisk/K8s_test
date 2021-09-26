4369563637247e18178a80d4e33d663384025e98
papazisk <papazisk@gmail.com>
2021-09-26 16:00:15 +0300

    Update check_namespaces.sh

--- .github/scripts/check_namespaces.sh
+++ .github/scripts/check_namespaces.sh
@@ -2,15 +2,15 @@
 set -eux
 set -o pipefail
 
-MONITORING=12
-VOTING=13
+#MONITORING=12
+#VOTING=13
 
-MONITORING=$(kubectl get ns | grep monitoring)
-if [ $? -ne 0 ]; then
+#MONITORING=$(kubectl get ns | grep monitoring)
+if [ -z $(kubectl get ns | grep monitoring) ]; then
   kubectl create ns monitoring
 fi
 
-VOTING=$(kubectl get ns | grep vote)
-if [ $? -ne 0 ]; then
-  kubectl create ns vote
-fi
+#VOTING=$(kubectl get ns | grep vote)
+#if [ $? -ne 0 ]; then
+#  kubectl create ns vote
+#fi
