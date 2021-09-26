ff1eb2258732af263a88ca7b883fcde0f7e2802f
papazisk <papazisk@gmail.com>
2021-09-26 14:02:57 +0300

    Update check_namespaces.sh

--- .github/scripts/check_namespaces.sh
+++ .github/scripts/check_namespaces.sh
@@ -6,3 +6,8 @@ MONITORING=$(kubectl get ns | grep monitoring)
 if [ $? -ne 0 ]; then
   kubectl create ns monitoring
 fi
+
+VOTING=$(kubectl get ns | grep vote)
+if [ $? -ne 0 ]; then
+  kubectl create ns vote
+fi
